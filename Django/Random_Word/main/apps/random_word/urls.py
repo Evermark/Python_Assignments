9409dcb5f6add361c5a891e84ee2eeb9691e4555
Evermark <mmathisen@gmail.com>
2018-02-14 11:55:13 -0800

    ...

--- Django/Random_Word/main/apps/random_word/urls.py
+++ Django/Random_Word/main/apps/random_word/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^random_word/$', views.index),
-    url(r'^random_word/generate/$', views.random_word),
-    url(r'^random_word/reset/$', views.reset)
+    url(r'^random_word/generate/$', views.random_word, name = 'generate'),
+    url(r'^random_word/reset/$', views.reset, name = 'reset')
   ]
