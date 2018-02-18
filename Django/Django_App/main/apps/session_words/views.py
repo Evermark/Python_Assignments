3e8b9759c6742b62b99ef0d263edf574e9940675
Evermark <mmathisen@gmail.com>
2018-02-18 14:15:03 -0800

    updated Session_Words unsure if works

--- Django/Django_App/main/apps/session_words/views.py
+++ Django/Django_App/main/apps/session_words/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render, redirect
 from datetime import datetime
 
-now = datetime.now()
 
 def index(request):
     return render(request, 'session_words/words.html')
 
 def add_words(request):
-    print 'got word'
+    # create table
+    if 'info' not in request.session:
+        request.session.['info'] = []
+    new_words = {
+        'word': request.POST['word']
+        'color': request.POST['color']
+        'time': datetime.now().strftime("%b %d, %Y %H:%M %p")
+    }
+    # set variable for font size checkbox
+    try:
+        request.POST['big']
+        new_words['big'] = 'on'
+    except:
+        pass
+    if 'big' not in request.POST:
+        new_word['big'] = 'off'
+    else:
+        new_word['big'] = 'on'
+    request.session['info'].append(new_words)
     request.session['word'] = request.POST['word']
     # request.session['color'] = request.POST['color']
     # request.session['size'] = request.POST['size']
-    context = {
-    "time": datetime.now().strftime("%b %d, %Y %H:%M %p")}
     return redirect('/session_words')
 
 def clear(request):
