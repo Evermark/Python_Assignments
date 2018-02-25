21588afd3f27db0425d31aa592ef8e4784222c03
Evermark <mmathisen@gmail.com>
2018-02-25 13:22:20 -0800

    new Courses App with messages

--- Django/Courses/Courses/apps/courses_app/views.py
+++ Django/Courses/Courses/apps/courses_app/views.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
 from .models import Course
 
 def index(request):
